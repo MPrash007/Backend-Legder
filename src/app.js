bbf276deb759875c41d3a756e745c5e87b698958
Prakash <prash8264@gmail.com>
2026-06-19 22:02:56 +0530

    Add welcome route to Ledger API for improved user experience

--- src/app.js
+++ src/app.js
@@ -21,6 +21,10 @@ const transactionRouter = require("./routes/transaction.routes")
  * - Use Routes
 */
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Ledger API")
+})
+
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRouter)
